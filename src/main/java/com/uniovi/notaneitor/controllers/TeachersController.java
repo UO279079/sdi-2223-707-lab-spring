e92846afb0b99badebde07aeb2afae30bd835ab2
uo279079 <uo279079@uniovi.es>
2023-02-16 10:32:11 +0100

    “SDI-2223-707-sesión2-Ejercicio-complementario2”

--- src/main/java/com/uniovi/notaneitor/controllers/TeachersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/TeachersController.java
@@ -3,33 +3,42 @@ package com.uniovi.notaneitor.controllers;
 import com.uniovi.notaneitor.entities.Teacher;
 import com.uniovi.notaneitor.services.TeachersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class TeachersController {
 
     @Autowired
     private TeachersService teachersService;
 
     @RequestMapping(value = "/teacher/list")
-    public String getList(){
-        return teachersService.getTeachers().toString();
+    public String getList(Model model){
+        model.addAttribute("teacherList", teachersService.getTeachers());
+        return "teacher/list";
+    }
+
+    @RequestMapping(value = "/teacher/add")
+    public String getTeacher(){
+        return "teacher/add";
     }
 
     @RequestMapping(value = "/teacher/add", method = RequestMethod.POST)
     public String setTeacher(@ModelAttribute Teacher teacher){
         teachersService.addTeacher(teacher);
-        return "Ok";
+        return "redirect:/teacher/list";
     }
 
     @RequestMapping(value = "/teacher/details/{id}")
-    public String getDetail(@PathVariable Long id){
-        return  teachersService.getTeacher(id).toString();
+    public String getDetail(Model model, @PathVariable Long id){
+        model.addAttribute("teacher", teachersService.getTeacher(id));
+        return "teacher/details";
     }
 
     @RequestMapping(value = "/teacher/delete/{id}")
-    public String deleteTeacher(@PathVariable Long id){
+    public String deleteTeacher(@PathVariable Long id) {
         teachersService.deleteTeacher(id);
-        return "Ok";
+        return "redirect:/teacher/list";
     }
 }
